060009ad3eca969b7bf2d50c9aa4f221e261745c
jefferson <jeffersondesouzacordel@gmail.com>
2016-09-14 22:07:41 -0300

    Prototipagem das confirmações, faltando as de exclusão em clientes, 7

---
+++ src/main/java/model/produto/ProdutoBean.java
@@ -0,0 +1,29 @@
+package model.produto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class ProdutoBean {
+
+	private List<Integer> produtosFiltrados;
+
+	public ProdutoBean() {
+		produtosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			produtosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
+
+}
